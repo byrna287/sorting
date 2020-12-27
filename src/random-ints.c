363a372359b3bef97ba86a15cdb519026249e814
Ailbhe Byrne <ailbhe.byrne287@mail.dcu.ie>
2020-12-27 15:00:24 +0000

    added different sorts to random-ints.c

--- src/random-ints.c
+++ src/random-ints.c
@@ -4,6 +4,7 @@
 #include <string.h>
 
 int compare_ints_asc(const void *a, const void *b);
+int compare_ints_desc(const void *a, const void *b);
 void print_nums(int numbers[], int len);
 
 int main(int argc, char *argv[]) {
@@ -11,6 +12,8 @@ int main(int argc, char *argv[]) {
    int len = atoi(argv[1]);
    int i, numbers[len];
 
+   // make a random array of numbers of size argv[1]
+   // have to have a seed for srand() or rand() will print the same numbers every time
    srand(time(0));
    for (i = 0; i < len; ++i) {
       numbers[i] = rand();
@@ -20,14 +23,25 @@ int main(int argc, char *argv[]) {
    if (strcmp(argv[2], "random") == 0) {
       print_nums(numbers, len);
    }
-
+   
+   // partially sorted
+   if (strcmp(argv[2], "partial") == 0) {
+      qsort (numbers, len / 2, sizeof (int), compare_ints_asc);
+      print_nums(numbers, len);
+   }
+   
+   // reverse sorted
+   if (strcmp(argv[2], "reverse") == 0) {
+      qsort (numbers, len, sizeof (int), compare_ints_desc);
+      print_nums(numbers, len);
+   }
+   
    // sorted
    if (strcmp(argv[2], "sorted") == 0) {
       qsort (numbers, len, sizeof (int), compare_ints_asc);
       print_nums(numbers, len);
    }
 
-   // reverse
 }
 
 int compare_ints_asc(const void *a, const void *b) {
@@ -38,7 +52,14 @@ int compare_ints_asc(const void *a, const void *b) {
    return (*da > *db) - (*da < *db);
 
 }
+int compare_ints_desc(const void *a, const void *b) {
 
+   const int *da = (const int *) a;
+   const int *db = (const int *) b;
+
+   return (*da < *db) - (*da > *db);
+
+}
 void print_nums(int numbers[], int len) {
 
    int i;
