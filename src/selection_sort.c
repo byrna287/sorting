c210241a62e0b87d6a29c027b08340094afef847
Ailbhe Byrne <ailbhe.byrne287@mail.dcu.ie>
2020-12-28 16:59:33 +0000

    added selection_sort.c and more tests to Makefile

---
+++ src/selection_sort.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void selection_sort(int nums[], int len);
+
+int main(int argc, char *argv[]) {
+
+   int len = atoi(argv[1]);
+   int *nums = malloc(len * sizeof(int));
+   int i;
+
+   for (i = 0; i < len; ++i) {
+      scanf("%d", &nums[i]);
+   }
+
+   selection_sort(nums, len);
+   
+   for (i = 0; i < len; ++i) {
+      printf("%d ", nums[i]);
+   }
+   free(nums);
+}
+
+void selection_sort(int nums[], int len) {
+
+   int i, j, smallest_ind, temp;
+
+   for (i = 0; i < len; ++i) {
+      smallest_ind = i;
+
+      for (j = i + 1; j < len; ++j) {
+         if (nums[j] < nums[smallest_ind]) {
+            smallest_ind = j;
+         }
+      }
+      temp = nums[smallest_ind];
+      nums[smallest_ind] = nums[i];
+      nums[i] = temp;
+   }
+}
